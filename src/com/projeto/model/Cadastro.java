507dcc4fb1e03c345671ecfd6758b2fe391df9e5
juuli <juuli@host.docker.internal>
2026-04-14 16:00:52 -0300

    primeiro commit

---
+++ src/com/projeto/model/Cadastro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projeto.model;
+
+public class Cadastro {
+    
+    private String email;
+    private String Tipo;
+
+    public Cadastro() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+}
